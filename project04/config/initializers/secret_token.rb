e13ac4bd845fd7f6f28e2b8f17e1c45988ea99dc
Tyler Thrailkill <tthrailk@mines.edu>
2013-02-24 23:39:42 -0700

    Project 04: Adding generated base rails application

---
+++ project04/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '14cd1719b6e1efdc9436ec8edd46fe782ac143fe5bdb95d413c36c545de130de897e01d7117f87b1539bb99f97c85125d63367c5b149aa8b690e88b26e55c698'
